91b8e4d514931766efdd89dfcfa2d5bd681bc0b3
Your Name <you@example.com>
2022-10-31 19:27:43 +0500

    git add .

--- brain_games/logic1.py
+++ brain_games/logic1.py
@@ -1,7 +1,7 @@
 import prompt
 import brain_games.games.logic_even
 
-def even():
+def even(game):
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
     print('Answer "yes" if the number is even, otherwise answer "no".')
@@ -9,11 +9,10 @@ def even():
 
     for i in range(3):
         answer = prompt.string(f'Question: {brain_games.games.logic_even.random_number}\nYour Answer: ')
-        if answer == brain_games.games.logic_even.correct_answer:
+        if answer == brain_games.games.logic_even.logic_even5():
             print('Correct!')
-        elif answer != brain_games.games.logic_even.correct_answer:
-            return print(f'{answer} is wrong answer ;(. Correct answer was {brain_games.games.logic_even.correct_answer}.')
+        elif answer != brain_games.games.logic_even.logic_even5():
+            return print(f'{answer} is wrong answer ;(. Correct answer was {brain_games.games.logic_even.logic_even5()}.')
         else:
             return print(f'{answer} is wrong answer ;(.')
     print(f'Congratulations, {name}!')
-
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -2,11 +2,11 @@
 
 
 from brain_games.logic1 import even
-
+from brain_games.games import logic_even
 
 def main():
     print('Welcome to the Brain Games!')
-    even()
+    even(logic_even)
 
 
 if __name__ == '__main__':
