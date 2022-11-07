78da0e7121848bb1b6666b4c49ecfdcaa3c114ba
Your Name <you@example.com>
2022-11-07 20:45:26 +0500

    add logic_gcd.py

--- brain_games/games/logic_gcd.py
+++ brain_games/games/logic_gcd.py
@@ -6,7 +6,7 @@ condition = 'Find the greatest common divisor of given numbers.'
 def function():
     number_1 = randint(1, 100)
     number_2 = randint(1, 100)
-    question = (number_1, number_2)
+    question = f'{number_1} {number_2}'
     while number_1 !=0 and number_2!=0:
         if number_1>number_2:
             number_1 = number_1 % number_2
